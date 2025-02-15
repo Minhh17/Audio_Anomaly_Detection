1462e0ec1dda8e55bda616aa8c0ea43b32715d24
Minhh17 <nguyenhaiminhr@gmail.com>
2025-02-15 11:25:20 +0700

    TestOrin

---
+++ config/__init__.py
@@ -0,0 +1,12 @@
+# from .default import get_cfg_defaults
+from .config_manager import ConfigManager
+
+__all__ = ['ConfigManager']
+
+# def update_config(cfg, yaml_file):
+#     '''
+#         This is a function that updates config from yaml file
+#     '''
+#     cfg.merge_from_file(yaml_file)
+#     cfg.freeze()
+#     return cfg
---
+++ core/Models/__init__.py
@@ -0,0 +1,18 @@
+from .vae import VariationalAutoEncoder
+from .vq_vae import VQ_VAE
+
+
+def get_model(cfg):
+    '''
+        Use parameters from config file to load initiate the right model
+    '''
+    name = cfg.MODEL.TYPE
+    if name == 'vae':
+        model = VariationalAutoEncoder(cfg.MODEL.VAE.OG_DIM)
+    elif name == 'vq_vae':
+        model = VQ_VAE()
+        print('VQ-VAE model is loaded.')
+    else:
+        raise ValueError(f'{name} is not yet implemented.')
+
+    return model
